c40ecd1102ac61f458459c05cec1bafb37bde370
taka01150810 <taka01150810@gmail.com>
2022-05-27 16:56:16 +0900

    Livewire($thisとwire:submit.prevent)

--- uReserve/app/Http/Livewire/Register.php
+++ uReserve/app/Http/Livewire/Register.php
@@ -7,9 +7,22 @@ use Livewire\Component;
 //php artisan make:livewire register で作成
 class Register extends Component
 {
+    public $name;
+    
+    public $email;
+    
+    public $password;
+
     public function register()
     {
-        dd('登録テスト');
+         dd($this);//$thisで$name,$email,$passwordの値を取得できる
+         /* 結果
+         ^ App\Http\Livewire\Register {#547 ▼
+            +name: "Takatoshi Miyanaka"
+            +email: "taka01150810@gmail.com"
+            +password: "aaa"
+            +id: "DmCCVBDHPlg0r7vd1kxm"
+         */
     }
 
     public function render()
--- uReserve/resources/views/livewire/register.blade.php
+++ uReserve/resources/views/livewire/register.blade.php
@@ -1,7 +1,13 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
     {{-- php artisan make:livewire register で作成--}}
-    <form wire:submit="register">
+    <form wire:submit.prevent="register">{{-- prevent ... ページ読み込みを防ぐ --}}
+        <label for="name">名前</label>
+        <input id="name" type="text" wire:model="name"><br>
+        <label for="email">メールアドレス</label>
+        <input id="email" type="email" wire:model="email"><br>
+        <label for="password">パスワード</label>
+        <input id="password" type="password" wire:model="password">
         <button>登録する</button>
     </form>
 </div>
