7ffb59eb45f11428890b3bfde6c773ce7cfe49b6
taka01150810 <taka01150810@gmail.com>
2022-05-27 17:32:16 +0900

    Livewire(エラーメッセージ 赤色)

--- uReserve/resources/views/livewire-test/register.blade.php
+++ uReserve/resources/views/livewire-test/register.blade.php
@@ -1,10 +1,12 @@
 <html>
 <head>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
     @livewireStyles
 </head>
 <body>
     Livewireテスト
-    
     @livewire('register');
 
     @livewireScripts
--- uReserve/resources/views/livewire/register.blade.php
+++ uReserve/resources/views/livewire/register.blade.php
@@ -4,13 +4,13 @@
     <form wire:submit.prevent="register">{{-- prevent ... ページ読み込みを防ぐ --}}
         <label for="name">名前</label>
         <input id="name" type="text" wire:model="name"><br>
-        @error('name') <div>{{ $message }} </div> @enderror
+        @error('name') <div class="text-red-500">{{ $message }} </div> @enderror
         <label for="email">メールアドレス</label>
         <input id="email" type="email" wire:model="email"><br>
-        @error('email') <div>{{ $message }} </div> @enderror 
+        @error('email') <div class="text-red-500">{{ $message }} </div> @enderror 
         <label for="password">パスワード</label>
         <input id="password" type="password" wire:model="password">
-        @error('password') <div>{{ $message }} </div> @enderror
+        @error('password') <div class="text-red-500">{{ $message }} </div> @enderror
         <button>登録する</button>
     </form>
 </div>
