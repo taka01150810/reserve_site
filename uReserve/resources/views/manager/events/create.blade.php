149d9b8beb68bc5a63b664d1eb961a20a04d7cc4
taka01150810 <taka01150810@gmail.com>
2022-05-31 15:39:29 +0900

    施設(新規登録 フォーム調整その1)

--- uReserve/resources/views/manager/events/create.blade.php
+++ uReserve/resources/views/manager/events/create.blade.php
@@ -8,9 +8,46 @@
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                日付 <input type="text" id="event_date" name="event_date">
-                開始時間 <input type="text" id="start_time" name="start_time">
-                終了時間 <input type="text" id="end_time" name="end_time"> 
+                <div class="max-w-2xl mx-auto">
+                    <x-jet-validation-errors class="mb-4" />
+
+                    @if (session('status'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('events.store') }}">
+                        @csrf
+                        
+                        <div>
+                            <x-jet-label for="event_name" value="イベント名" />
+                            <x-jet-input id="event_name" class="block mt-1 w-full" type="text" name="event_name" :value="old('even')" required autofocus />
+                        </div>
+
+
+                        <div class="md:flex justify-between">
+                            <div class="mt-4">
+                                <x-jet-label for="event_date" value="イベント日付" />
+                                <x-jet-input id="event_date" class="block mt-1 w-full" type="event_date" name="event_date" required />
+                            </div>
+                            <div class="mt-4">
+                                <x-jet-label for="start_time" value="開始時間" />
+                                <x-jet-input id="start_time" class="block mt-1 w-full" type="start_time" name="start_time" required />
+                            </div>
+                            <div class="mt-4">
+                                <x-jet-label for="end_time" value="終了時間" />
+                                <x-jet-input id="end_time" class="block mt-1 w-full" type="end_time" name="end_time" required />
+                            </div>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-jet-button class="ml-4">
+                                新規登録
+                            </x-jet-button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
