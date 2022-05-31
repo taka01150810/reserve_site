bc8ba922cbbfa6b83138e3981d178571a64f8960
taka01150810 <taka01150810@gmail.com>
2022-05-31 10:59:41 +0900

    施設(ダミーデータ)

--- uReserve/database/factories/EventFactory.php
+++ uReserve/database/factories/EventFactory.php
@@ -18,8 +18,16 @@ class EventFactory extends Factory
      */
     public function definition()
     {
+        //今月分のダミーデータを作成
+        $dummyDate = $this->faker->dateTimeThisMonth;
+
         return [
-            //
+            'name' => $this->faker->name,
+            'information' => $this->faker->realText,
+            'max_people' => $this->faker->numberBetween(1,20),
+            'start_date' => $dummyDate->format('Y-m-d H:i:s'),//開始日時
+            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),//終了日時
+            'is_visible' => $this->faker->boolean
         ];
     }
 }
--- uReserve/database/seeders/EventSeeder.php
+++ uReserve/database/seeders/EventSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Event;
 
 //php artisan make:model Event -a で作成
 class EventSeeder extends Seeder
