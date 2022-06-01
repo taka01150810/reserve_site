4c92f54c50397db5da6fc39ba83d2de494792f5e
taka01150810 <taka01150810@gmail.com>
2022-06-01 11:22:02 +0900

    施設(バリデーション)

--- uReserve/app/Http/Requests/StoreEventRequest.php
+++ uReserve/app/Http/Requests/StoreEventRequest.php
@@ -26,6 +26,13 @@ class StoreEventRequest extends FormRequest
     {
         return [
             //
+            'event_name' => ['required', 'max:50'],
+            'information' => ['required', 'max:200'],
+            'event_date' => ['required', 'date'],
+            'start_time' => ['required'],
+            'end_time' => ['required', 'after:start_time'],
+            'max_people' => ['required', 'numeric', 'between:1,20'],
+            'is_visible' => ['required', 'boolean'],
         ];
     }
 }
