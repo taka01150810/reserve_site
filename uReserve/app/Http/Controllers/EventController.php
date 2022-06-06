972c1f0076348d5d91caad16a1df845e020e21d4
taka01150810 <taka01150810@gmail.com>
2022-06-06 14:01:41 +0900

    施設(予約人数の表示 (index, past))

--- uReserve/app/Http/Controllers/EventController.php
+++ uReserve/app/Http/Controllers/EventController.php
@@ -176,7 +176,16 @@ class EventController extends Controller
     public function past()
     {
         $today = Carbon::today();
+    
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->groupBy('event_id');
+
         $events = DB::table('events')
+        ->leftJoinSub($reservedPeople, 'reservedPeople',
+        function($join){
+            $join->on('events.id', '=', 'reservedPeople.event_id');
+        })
         ->whereDate('start_date', '<', $today )
         ->orderBy('start_date', 'desc')
         ->paginate(10);
