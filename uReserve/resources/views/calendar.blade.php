4612498208a7f206a7e60322a2410b4e83433d49
taka01150810 <taka01150810@gmail.com>
2022-06-08 15:32:51 +0900

    利用者(Bladeコンポーネント(day, calendar-time))

--- uReserve/resources/views/calendar.blade.php
+++ uReserve/resources/views/calendar.blade.php
@@ -6,14 +6,12 @@
     </x-slot>
 
     <div class="py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="event-calendar border border-red-400 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="max-w-2xl mx-auto py-4">
                     @livewire('calendar')
                     {{--  結果
                         https://i.gyazo.com/c50dc8ce835b91ae1bdc962b922e6a2c.png 
                     --}}
-                </div>
             </div>
         </div>
     </div>
--- uReserve/resources/views/livewire/calendar.blade.php
+++ uReserve/resources/views/livewire/calendar.blade.php
@@ -1,19 +1,21 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
     {{-- php artisan make:livewire Calendar で作成 --}}
-    カレンダー
-    <input id="calendar" class="block mt-1 w-full" type="text" name="calendar"
+    <div class="text-center text-sm">
+        日付を選択してください。本日から最大30日先まで選択可能です。
+    </div> 
+    <input id="calendar" class="block mt-1 mb-2 mx-auto" type="text" name="calendar"
     {{-- datepickerを変更したら値も変える --}}
     value="{{ $currentDate }}" wire:change="getDate($event.target.value)"/>
-    {{ $currentDate }}
-    <div class="flex">
-        @for($day = 0; $day < 7; $day++)
-        {{ $currentWeek[$day] }}
-        @endfor
-        {{-- 結果
-            2022-06-09(指定した日付)
-            06月09日 06月10日 06月11日 06月12日 06月13日 06月14日 06月15日(指定した日付+7日分)
-        --}}
+    <div class="flex border border-green-400 mx-auto">
+        <x-calendar-time />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
     </div>
     @foreach($events as $event)
     {{ $event->start_date }}<br>
