0c3e3f5f2e2d2d409c05a47e167ab888eda917e5
taka01150810 <taka01150810@gmail.com>
2022-06-08 15:13:56 +0900

    利用者(Calendarレイアウト)

--- uReserve/resources/views/calendar.blade.php
+++ uReserve/resources/views/calendar.blade.php
@@ -1,26 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<x-calendar-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            イベントカレンダー
+        </h2>
+    </x-slot>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        @livewire('calendar')
-        <script src="{{ mix('js/flatpickr.js')}}"></script>
-        @livewireScripts
-    </body>
-</html>
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="max-w-2xl mx-auto py-4">
+                    @livewire('calendar')
+                    {{--  結果
+                        https://i.gyazo.com/c50dc8ce835b91ae1bdc962b922e6a2c.png 
+                    --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-calendar-layout>
