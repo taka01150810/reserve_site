f7f093150ca05add18562574c410a84a793613a3
taka01150810 <taka01150810@gmail.com>
2022-06-08 15:51:23 +0900

    利用者(判定用データ用意 (定数など))

--- uReserve/app/Http/Livewire/Calendar.php
+++ uReserve/app/Http/Livewire/Calendar.php
@@ -13,6 +13,10 @@ class Calendar extends Component
 
     public $day;
 
+    public $checkDay;
+
+    public $dayOfWeek;
+
     public $currentWeek;
 
     public $sevenDaysLater;
@@ -41,18 +45,41 @@ class Calendar extends Component
         for($i = 0; $i < 7; $i++ )
         {
             $this->day = CarbonImmutable::today()->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName; 
+            array_push($this->currentWeek,[// 連想配列に変更
+                'day' => $this->day,//カレンダー表示用(○月△日)
+                'checkDay' => $this->checkDay, //判定用(○○○○-△△-□□)
+                'dayOfWeek' => $this->dayOfWeek //曜日
+            ]
+            );
         }
-        // dd($this->currentWeek);
+        //dd($this->currentWeek);
         /* 結果
-        array:7 [▼
-            0 => "06月07日"
-            1 => "06月08日"
-            2 => "06月09日"
-            3 => "06月10日"
-            4 => "06月11日"
-            5 => "06月12日"
-            6 => "06月13日"
+        ^ array:7 [▼
+        0 => array:3 [▼
+            "day" => "06月08日"
+            "checkDay" => "2022-06-08"
+            "dayOfWeek" => "水曜日"
+        ]
+        1 => array:3 [▼
+            "day" => "06月09日"
+            "checkDay" => "2022-06-09"
+            "dayOfWeek" => "木曜日"
+        ]
+        2 => array:3 [▼
+            "day" => "06月10日"
+            "checkDay" => "2022-06-10"
+            "dayOfWeek" => "金曜日"
+        ]
+        3 => array:3 [▼
+            "day" => "06月11日"
+            "checkDay" => "2022-06-11"
+            "dayOfWeek" => "土曜日"
+        ]
+        4 => array:3 [▶]
+        5 => array:3 [▶]
+        6 => array:3 [▶]
         ]
         */
     }
@@ -72,7 +99,13 @@ class Calendar extends Component
         for($i = 0; $i < 7; $i++ )
         {
             $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('m月d日'); // parseでCarbonインスタンスに変換後 日付を加算
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName; 
+            array_push($this->currentWeek,[// 連想配列に変更
+                'day' => $this->day,//カレンダー表示用(○月△日)
+                'checkDay' => $this->checkDay, //判定用(○○○○-△△-□□)
+                'dayOfWeek' => $this->dayOfWeek //曜日
+            ]);
         }
     }
 
