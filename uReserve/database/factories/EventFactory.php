512e699515b2687a32f22d3b18d85fbbf353458d
taka01150810 <taka01150810@gmail.com>
2022-06-08 15:01:29 +0900

    利用者(ダミーデータの修正(分と時間を画面に合うように変更))

--- uReserve/database/factories/EventFactory.php
+++ uReserve/database/factories/EventFactory.php
@@ -18,16 +18,34 @@ class EventFactory extends Factory
      */
     public function definition()
     {
+
+        $availableHour = $this->faker->numberBetween(10, 18); //10時～18時
+        $minutes = [0, 30]; // 00分か 30分
+        $mKey = array_rand($minutes); //ランダムにキーを取得
+        $addHour = $this->faker->numberBetween(1, 3); //イベント時間 1時間～3時間
+
         //今月分のダミーデータを作成
         $dummyDate = $this->faker->dateTimeThisMonth;
 
+        $startDate = $dummyDate->setTime($availableHour, $minutes[$mKey]);
+        $clone = clone $startDate; //そのままmodifyするとstartDateも変わるためコピー
+        $endDate = $clone->modify('+'.$addHour.'hour');
+        //dd($startDate, $endDate);
+        /* 結果 1〜3時間の間で30分単位で表示
+        ^ DateTime @1654660800 {#1924
+        date: 2022-06-08 13:00:00.0 Asia/Tokyo (+09:00)
+        }
+        ^ DateTime @1654664400 {#1926
+        date: 2022-06-08 14:00:00.0 Asia/Tokyo (+09:00)
+        }
+        */
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1,20),
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),//開始日時
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),//終了日時
-            'is_visible' => $this->faker->boolean
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'is_visible' => $this->faker->boolean,
         ];
     }
 }
