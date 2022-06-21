67bc1c4205924c27ae69b66f575da6dc809534fc
taka01150810 <taka01150810@gmail.com>
2022-06-21 11:57:32 +0900

    利用者(予約可能な人数の表示)

--- uReserve/app/Http/Controllers/ReservationController.php
+++ uReserve/app/Http/Controllers/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Event;
 
 //php artisan make:controller ReservationController で生成
@@ -17,6 +18,21 @@ class ReservationController extends Controller
     public function detail($id)
     {
         $event = Event::findOrFail($id);
-        return view('event-detail', compact('event'));
+
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id')
+        ->having('event_id', $event->id )//havingはgroupByの後に検索
+        ->first();
+
+        //予約可能な人数=最大定員-予約済みの人数(キャンセルを除く)
+        if(!is_null($reservedPeople)){
+            $resevablePeople = $event->max_people - $reservedPeople->number_of_people;
+        } else {
+            $resevablePeople = $event->max_people;
+        } 
+
+        return view('event-detail', compact('event', 'resevablePeople'));
     }
 }
--- uReserve/resources/views/event-detail.blade.php
+++ uReserve/resources/views/event-detail.blade.php
@@ -56,6 +56,15 @@
                                 <x-jet-label for="max_people" value="定員数" />
                                 {{ $event->max_people }}
                             </div>
+                            <div class="mt-4">
+                                <x-jet-label for="reserved_people" value="予約人数" />
+                                <select name="reserved_people">
+                                @for($i = 1; $i <= $resevablePeople; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                            </select>
+                            </div>
+                            {{-- 結果 https://gyazo.com/9c0e4b2dc2a9f253b8fd4c7fb7bb0cfd --}}
                             <x-jet-button class="ml-4">
                              予約する
                             </x-jet-button>
