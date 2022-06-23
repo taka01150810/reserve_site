ae32f54c1895ae38d50b982feec9c234b549c339
taka01150810 <taka01150810@gmail.com>
2022-06-23 14:58:35 +0900

    利用者(マイページindex)

--- uReserve/routes/web.php
+++ uReserve/routes/web.php
@@ -48,6 +48,7 @@ Route::prefix('manager')
 Route::middleware('can:user-higher')->group(function(){
     Route::get('/dashboard', [ReservationController::class, 'dashboard' ])->name('dashboard');
     Route::get('mypage', [MyPageController::class, 'index' ])->name('mypage.index');
+    Route::get('mypage/{id}', [MyPageController::class, 'show' ])->name('mypage.show');
     Route::get('/{id}', [ ReservationController::class, 'detail' ] )->name('events.detail');
     Route::post('/{id}', [ ReservationController::class, 'reserve' ] )->name('events.reserve');
 });
